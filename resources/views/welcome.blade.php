1c90c21e0239aae962abdd7d7df2eebfe40b9b21
Paulo Tamanini <tamanini8@gmail.com>
2024-03-20 17:00:35 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('styles.css')}}">
+    <title>Template 4</title>
+</head>
+<body>
+
+<div class="container-site">
+    <header>
+        <div class="container-header max-width min-width">
+            <div class="container-logo">
+                <img src="{{asset('img/logo.png')}}" alt="logo">
+                <p>goodmoney</p>
+            </div>
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">about us</a></li>
+                <li><a href="#">contact</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <main>
+        <div class="container-main max-width min-width">
+            <p>main</p>
+        </div>
+    </main>
+
+    <footer>
+        <div class="container-footer max-width min-width">
+            <p>footer</p>
+        </div>
+
+    </footer>
+</div>
+
+
+</body>
+</html>
